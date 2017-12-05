1045e5b68c288bb1cf653ec710422eae3ea44e52
Ann Edvinsson <aslin.ann@gmail.com>
2017-12-05 16:09:02 +0100

    created files

---
+++ main.js
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <link rel="stylesheet" href="memory.css">
+</head>
+<body>
+<div id="memory_board">
+
+
+
+
+  <div class="card" data-card="0"><img src="img/fem1.jpg" alt="">0</div>
+  <div class="card" data-card="0"><img src="img/fem1.jpg" alt="">0</div>
+  <div class="card" data-card="1"><img src="img/fem2.jpg" alt="">1</div>
+  <div class="card" data-card="1"><img src="img/fem2.jpg" alt="">1</div>
+  <div class="card" data-card="2"><img src="img/fem3.jpeg" alt="">2</div>
+  <div class="card" data-card="2"><img src="img/fem3.jpeg" alt="">2</div>
+  <div class="card" data-card="3"><img src="img/fem4.jpg" alt="">3</div>
+  <div class="card" data-card="3"><img src="img/fem4.jpg" alt="">3</div>
+  <div class="card" data-card="4"><img src="img/fem5.jpg" alt="">4</div>
+  <div class="card" data-card="4"><img src="img/fem5.jpg" alt="">4</div>
+  <div class="card" data-card="5"><img src="img/fem6.jpg" alt="">5</div>
+  <div class="card" data-card="5"><img src="img/fem6.jpg" alt="">5</div>
+  <div class="card" data-card="6"><img src="img/fem7.jpg" alt="">6</div>
+  <div class="card" data-card="6"><img src="img/fem7.jpg" alt="">6</div>
+  <div class="card" data-card="7"><img src="img/fem8.jpg" alt="">7</div>
+  <div class="card" data-card="7"><img src="img/fem8.jpg" alt="">7</div>
+</div>
+
+
+<script src="main.js" charset="utf-8"></script>
+</body>
+</html>
