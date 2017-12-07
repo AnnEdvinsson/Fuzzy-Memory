058f218bc3a70fbd93513292db254d979ed8937b
Ann Edvinsson <aslin.ann@gmail.com>
2017-12-07 21:42:49 +0100

    autoindent

--- main.js
+++ main.js
@@ -2,10 +2,9 @@
 
 let cards = document.querySelectorAll('.card');
 // console.log(cards);
-let compareArray = []; //array där vi jämför kort
+let compareArray = []; //array to compare cards
 let divArray = [];
-let matchPair = 0; //antalet matchade par
-
+let matchPair = 0; //number of matched pairs
 let memory_board =document.querySelector('#memory_board');
 
 function resetBoard(){
@@ -13,18 +12,17 @@ function resetBoard(){
     memory_board.appendChild(memory_board.children[Math.random() * i | 0]); //randomiserar divarna
   }
   cards.forEach(function(card){
-  card.querySelector('img').classList.remove('images_clicked');
-  card.classList.remove('clicked');
-  console.log(card);
+    card.querySelector('img').classList.remove('images_clicked');
+    card.classList.remove('clicked');
+    console.log(card);
 
-});
-  compareArray = []; //tömmer arrayen efter valt två kort
+  });
+  compareArray = []; //empties the array after picking two cards
   divArray = [];
 }
 
-
 for (var i = memory_board.children.length; i >= 0; i--) {
-  memory_board.appendChild(memory_board.children[Math.random() * i | 0]); //randomiserar divarna
+  memory_board.appendChild(memory_board.children[Math.random() * i | 0]); //ramdomizing the divs
 }
 
 let comparefunc = (dataset) => {
@@ -39,55 +37,45 @@ let comparefunc = (dataset) => {
 
     if (compareArray[0]==compareArray[1]) {
       console.log('match');
-      matchPair++; //lägger till 1 för varje matchande par i matchPair-variabeln.
+      matchPair++; //adds one for each matching pair
       console.log(matchPair);
-      compareArray = []; //tömmer arrayen efter valt två kort
+      compareArray = []; //empties the array after picking two cards
       divArray = [];
-      if (matchPair == 8) { //när vi hittat alla matchande par
-        console.log('You won');
+      if (matchPair == 8) { //when the player found all matching pairs
+        // console.log('You won');
       }
-
     } else {
-      console.log('no match');
-
+      // console.log('no match');
       setTimeout(function() {
-        divArray[0].classList.remove('clicked'); //tar bort klassen clicked på klickad div.
+        divArray[0].classList.remove('clicked'); //removes the class clicked on the picked divs
         divArray[1].classList.remove('clicked');
-        divArray[0].querySelector('img').classList.remove('images_clicked');
+        divArray[0].querySelector('img').classList.remove('images_clicked');//removes the class images_clicked on the picked divs
         divArray[1].querySelector('img').classList.remove('images_clicked');
-
-        compareArray = []; //tömmer arrayen efter valt två kort
+        compareArray = []; //empties the array after picking two cards
         divArray = [];
       }, 1000)
     }
-    // compareArray = []; //tömmer arrayen efter valt två kort
   }
 }
 
 cards.forEach((cardEach)=> {
   // console.log(cardEach);
-
   cardEach.addEventListener('click', (event) => {
     let currentDiv = event.target;
     divArray.push(currentDiv);
     console.log(currentDiv);
     currentDiv.classList.toggle('clicked');
-
     let currentImg = currentDiv.querySelector('img');
     currentImg.classList.add('images_clicked');
-
     // console.log(event.target.dataset.card);
     let currentCard = event.target.dataset.card;
     console.log(currentCard);
-    let compare = compareArray.push(currentCard); //för att kunna jämföra korten.
-                                                  //först dit jag vill skicka och
-                                                  //inom parates det jag vill skicka
+    let compare = compareArray.push(currentCard);
     // console.log(compareArray);
-
     return comparefunc(currentCard);
   })
 });
 
-  document.querySelector(".playBtn").addEventListener('click', function(){
-    return resetBoard();
-  });
+document.querySelector(".playBtn").addEventListener('click', function(){
+  return resetBoard(); //when player clicks the Reset button the game board shuffles and reset
+});
